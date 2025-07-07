246a51f19e4291253ae551eb226060041b1e6ccd
aanandmahawal <151204835+aanandmahawal@users.noreply.github.com>
2025-07-07 15:34:38 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,20 +1,20 @@
-import pandas as pd
 import numpy as np
-import streamlit as st
+import pandas as pd
 
-@st.cache_data
 def load_center():
-    df = pd.read_csv("df_clean.csv")
-    center_lat = df['lat'].mean()
-    center_lon = df['lon'].mean()
-    return center_lat, center_lon
+    center = np.load("center_point.npz")
+    return center['center_lat'], center['center_lon']
 
 def prepare_input(lat, lon, hour):
     center_lat, center_lon = load_center()
+    dist = np.sqrt((lat - center_lat) ** 2 + (lon - center_lon) ** 2)
+    angle = np.arctan2(lat - center_lat, lon - center_lon)
+    hour_sin = np.sin(2 * np.pi * hour / 24)
+    hour_cos = np.cos(2 * np.pi * hour / 24)
     return pd.DataFrame([{
-        'dist_from_center': np.sqrt((lat - center_lat)**2 + (lon - center_lon)**2),
-        'angle_from_center': np.arctan2(lat - center_lat, lon - center_lon),
-        'hour_sin': np.sin(2 * np.pi * hour / 24),
-        'hour_cos': np.cos(2 * np.pi * hour / 24),
+        'dist_from_center': dist,
+        'angle_from_center': angle,
+        'hour_sin': hour_sin,
+        'hour_cos': hour_cos,
         'hour': hour
     }])
